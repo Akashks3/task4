c7cc263b1832b1a5504f63571d6b506b8dd888b9
Akashks3 <155128336+Akashks3@users.noreply.github.com>
2024-04-29 22:11:45 +0530

    first commit

---
+++ resume.js
@@ -0,0 +1,51 @@
+
+//json resume
+
+resumedata={
+    "about me":[
+        { 
+"about":"BCA fresher with a strong in computer application ,programming languages,and technical skills.I'M self learner and team player eager to contribute to a organization's success by leveraging my knowledge and passion for technology.Motivated to learn and grow ,seeking a challenging opportunity to apply my skills and gain practical experience in the field of computer",
+}
+],
+"contact":[
+    {
+    "name":"akash",
+    "email": "saraakas4342@gmail.com",
+    "phone": "6383221660",
+    "url":"https://jopubop03u7etplptg3uba.on.drv.tw/www.akaskcv.com/",
+}
+],
+    "CERTIFICATION":[
+      {
+        "CERTIFICATION 1":"Javascript Infosys on sep 2023",
+        "CERTIFICATION 2":"google for education partner cerificated for Python on aug 2023",
+      }
+    ],
+    "education": [
+      {
+        "Institution": "University of madras",
+        "degree": "Bachelor of computer application",
+        "name of collage": "drbccc hindu collage",
+        "name of ":"",
+      }
+    ],
+    "hard skills":[
+      "JavaScript",
+      "mysql",
+      "Node.js",
+      "HTML",
+      "php",
+      "CSS"
+    ],
+    "soft skills":[
+        "Teamwork & communication",
+              "Self-learning",
+              "Time management",
+            ],
+    "project":[
+        "Web Curriculum vitae",
+        "Vaccination Alert For Infants portal",
+
+    ],
+  }
+  
